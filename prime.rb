94eaf4fade4856134407acebd3d0d2f4958712af
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-25 16:18:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    return false
+  elsif num > 2
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+  end
+  true
+end
